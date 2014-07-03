8fa2b2f95b30e6348d51992ce13084a077107df8
Zander Hill <zhill@rentpath.com>
2014-07-03 15:20:32 -0400

    [ZH][74388908] Add route to version path '/v1/xxx.json'
    Pivotal Story: https://www.pivotaltracker.com/story/show/74388908

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  api_version(:default => true, :module => "V1", :header => {:name => "Accept", :value => "application/vnd.primedia.com; version=1"}, :parameter => {:name => "version", :value => "1"}) do
+  api_version(:default => true, :module => "V1",  :path => {:value => "v1"}, :header => {:name => "Accept", :value => "application/vnd.primedia.com; version=1"}, :parameter => {:name => "version", :value => "1"}) do
     resources :applications
   end
 
