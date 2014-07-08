42cb79df976cc00c7fde18352cde5a7c789aa25f
Zander Hill <zhill@rentpath.com>
2014-07-08 16:15:19 -0400

    [ZH][74627826] Update seed.rb
    Pivotal Story: https://www.pivotaltracker.com/story/show/74627826

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,46 +13,62 @@ yaml = YAML.load <<-YML
 ---
 ag:
   id: ag
+  primary_team: web
+  host_group: "ag"
   github_repository: ag
   name: Apartment Guide
   wiki_path: IDG/Releases/Web/ApartmentGuide
 ag_console:
   id: ag_console
+  primary_team: web
+  host_group: "ag"
   github_repository: ag-console
   name: AG Sites Console
   wiki_path: IDG/Releases/B2B_Services/AG_Sites_Console
 ag_console_server:
   id: ag_console_server
+  primary_team: web
+  host_group: "ag"
   github_repository: ag-console-server
   name: AG Sites Server
   wiki_path: IDG/Releases/B2B_Services/AG_Sites_Server
 portal:
   id: portal
+  primary_team: iws
+  host_group: "ag"
   github_repository: portal
   name: Portal
   wiki_path: IDG/Releases/B2B_Services/Portal
 randr:
   id: randr
+  primary_team: iws
+  host_group: "ag"
   github_repository: randr
   name: Ratings and Reviews Service (randr)
   wiki_path: IDG/Releases/B2B_Services/Randr
 rnr_ui:
   id: rnr_ui
+  primary_team: iws
+  host_group: "ag"
   github_repository: rnr_ui
   name: Ratings and Reviews UI (rnr_ui)
   wiki_path: IDG/Releases/B2B_Services/Rating_Reviews
 max_leases_2:
   id: max_leases_2
+  primary_team: iws
+  host_group: "ag"
   github_repository: maxleases2
   name: Max Leases 2
   wiki_path: IDG/Releases/B2B_Services/MaxLeases2
   YML
 
   yaml.values.each do |rec|
-    metadata = {wiki_generator: {template_path: rec["wiki_path"],
-                                 github_repository: rec["github_repository"]}}.to_json
+    metadata = {wiki_generator: {github_repository: rec["github_repository"]}}
     App.create(name: rec["id"],
                     description: rec["name"],
+                    host_group: rec["host_group"],
+                    primary_team: rec["primary_team"],
+                    wiki_path: rec['wiki_path'],
                     metadata: metadata)
 
   end
